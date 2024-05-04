051f7eeac3c370372860d1b38dc56a93588a02d7
makano <makanobush@gmail.com>
2024-05-04 22:08:12 +0300

    Cloudflare AI

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,17 @@ const app = new Hono()
 
 app.use(renderer)
 
-app.get('/', (c) => {
-  return c.render(<h1>Hello!</h1>)
+// app.get('/', (c) => {
+//   return c.render(<h1>Hello!</h1>)
+// })
+
+app.get('/', async (c) => {
+  return new Response(await env.AI.run('@hf/thebloke/neural-chat-7b-v3-1-awq', {
+  	messages: c.req.param('messages'),
+  	stream: true
+  }), {
+	headers: { 'content-type': 'text/event-stream' },
+  });
 })
 
 export default app
