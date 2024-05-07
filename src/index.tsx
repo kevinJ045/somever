34fd53be357a6628fec993570c620e2cc1bd4604
makano <makanobush@gmail.com>
2024-05-07 21:35:42 +0300

    e

--- src/index.tsx
+++ src/index.tsx
@@ -19,9 +19,11 @@ app.get('/', (c) => {
 app.post('/ai', async (c) => {
   const body = await c.req.json();
   console.log(body);
-  const messages = [...body.system, ...body.user];
+  const messages = [...body.messages];
+  const prompt = body.prompt;
   return c.body(await c.env.AI.run('@hf/thebloke/neural-chat-7b-v3-1-awq', {
-  	messages
+  	messages,
+  	prompt
   }), 200);
 })
 
