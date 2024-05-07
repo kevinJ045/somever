1ddaee0938d21077af9b5a497eb93099091ce04f
makano <makanobush@gmail.com>
2024-05-07 22:13:07 +0300

    m

--- src/index.tsx
+++ src/index.tsx
@@ -11,18 +11,25 @@ interface Env {
 
 app.use(renderer)
 
-app.get('/', (c) => {
-   return c.render(<h1>Hello!</h1>)
-})
-
+app.get('/ai', async (c) => {
+	return c.render(<div>
+		<h1>AI Options</h1>
+		<ul>
+			<li><code>messages</code>(only if no prompt): An array of <code>{ role: 'system' | 'user', content: 'string' }</code></li>
+			<li><code>prompt</code>(only if no messages): A 'string'</li>
+			<li><code>model</code>(optional): A 'string' for the model name</li>
+		</ul>
+	</div>);
+});
 
 app.post('/ai', async (c) => {
   const body = await c.req.json();
   const messages = body.messages;
   const prompt = body.prompt;
+  let model = body.model || '@hf/thebloke/neural-chat-7b-v3-1-awq';
   const options = prompt ? { prompt } : { messages };
-  const data = await c.env.AI.run('@hf/thebloke/neural-chat-7b-v3-1-awq', options);
+  const data = await c.env.AI.run(model, options);
   return c.body(data.response, 200);
-})
+});
 
 export default app
